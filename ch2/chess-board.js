0cc30dd1dfaba083092025ea034ed5df85340a0f
nsm <nsm@riseup.net>
2017-09-06 22:12:56 -0400

    finish ch2

---
+++ ch2/chess-board.js
@@ -0,0 +1,14 @@
+let size = 20;
+let line = '';
+let offset = 0;
+
+for (let i = 1; i <= size * size; i++) {
+	if ((i + offset) % 2 === 0) line += '#';
+	else if ((i + offset) % 2 === 1) line += ' ';
+	if (i % size === 0) {
+		offset = 1 - offset;
+		line += '\n';
+	}
+}
+
+console.log(line);
